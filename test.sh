1c22fdc059bc0f750d6edac3f257e261e37ec968
Charlie McClung <charlie@cmr1.com>
2017-03-24 20:28:40 -0600

    Actually test ...

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-curl -I http://127.0.0.1
-
-curl -I http://127.0.0.1 | head -n 1 | cut -d$' ' -f2
-
 ([[ "$(curl -Is http://127.0.0.1 | head -n 1|cut -d$' ' -f2)" == "200" ]] && echo "Success!") || echo "Failed!"
