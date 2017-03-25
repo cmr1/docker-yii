fe2f939f3f816a600e918915564cebf5941193ab
Charlie McClung <charlie@cmr1.com>
2017-03-24 20:12:54 -0600

    Update test script

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-[[ "$(curl -Is http://127.0.0.1 | head -n 1|cut -d$' ' -f2)" == "200" ]]
+curl -I http://127.0.0.1
+
+curl -I http://127.0.0.1 | head -n 1 | cut -d$' ' -f2
+
+([[ "$(curl -Is http://127.0.0.1 | head -n 1|cut -d$' ' -f2)" == "200" ]] && echo "Success!") || echo "Failed!"
